9bbbc7cbccf38a8ac93b0dc8b28cc7d97df7abee
harshit <harshitkhandelwal830@gmail.com>
2019-02-19 21:37:44 +0530

    commit

--- knn/_knn/lib/libfast_knn.cpp
+++ knn/_knn/lib/libfast_knn.cpp
@@ -1,20 +1,4 @@
-/*
-#    Copyright (C) 2017 libfast_knn authors(Antonio Costa, Cooler_),
-#    
-#    This file is part of libfast_knn
-#    
-#    libfast_knn is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    libfast_knn is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+
 #include "libfast_knn.h"
 
 // calc matrix distance
