f8a10a96729f87da0a5b1dee6ed843673450dc5f
harshit <harshitkhandelwal830@gmail.com>
2019-03-02 17:38:35 +0530

    knn

--- src/knn/_knn/knn.cpp
+++ src/knn/_knn/knn.cpp
@@ -1,7 +1,114 @@
+#ifndef COLLECT_H
+#define COLLECT_H
 
-#include "libfast_knn.h"
+#include <iostream>
+#include <assert.h>
+#include <vector>
+#include <time.h>
+#include <armadillo>
+#include <map>
+#ifndef NORMALIZERDATA_H
+#define NORMALIZERDATA_H
 
-// calc matrix distance
+#include <armadillo>
+#include <iostream>
+using arma::Mat;
+using arma::Row;
+
+
+template <class T> class NormalizerData
+{
+private:
+    Row<T> ranges;
+    Row<T> less_values;
+    Row<T> max_values;
+
+public:
+    Row<T> Brick_limits();
+    Row<T> Brick_max_values();
+    Row<T> Brick_less_values();
+
+    Mat<T> normalize(Mat<T> table_data);
+    Row<T> normalize(Row<T> table_data2);
+};
+
+template<class T> Row<T> NormalizerData<T>::Brick_max_values()
+{
+    return max_values;
+}
+
+template<class T> Row<T> NormalizerData<T>::Brick_less_values()
+{
+    return less_values;
+}
+
+template<class T> Row<T> NormalizerData<T>::Brick_limits()
+{
+    return ranges;
+}
+
+template<class T> arma::Row<T> NormalizerData<T>::normalize(arma::Row<T> table_data4)
+{
+    return (table_data4-less_values)/ranges;
+}
+
+template<class T> arma::Mat<T> NormalizerData<T>::normalize(arma::Mat<T> table_data3)
+{
+    less_values=arma::min(table_data3);
+    max_values=arma::max(table_data3);
+    ranges=max_values-less_values;
+    arma::uword m=table_data3.n_rows;
+    arma::Mat<T> norm_table_data=table_data3-repmat(less_values, m, 1);
+    norm_table_data=norm_table_data/repmat(ranges, m, 1);
+
+    return norm_table_data;
+}
+
+#endif 
+
+#define DEBUG_FASTKNN(x, s...) do { \
+ time_t t = time(NULL); \
+ char *d = ctime(&t); \
+ fprintf(stderr, "\n--- DEBUG-START ---\n\n %.*s %s[%d] %s(): \n", \
+ (int)strlen(d) - 1, d, __FILE__, \
+ __LINE__, __FUNCTION__); \
+ fprintf(stderr, x, ## s); \
+ fprintf(stderr,"\n\n--- DEBUG-END ---\n"); \
+} while (0);
+ 
+struct MemoryStruct {
+  char *memory;
+  size_t size;
+};
+
+using namespace::std;
+using arma::Row;
+using arma::Mat;
+using arma::endr;
+using arma::colvec;
+using arma::rowvec;
+using arma::ucolvec;
+using arma::mat;
+using arma::umat;
+using arma::uword;
+
+class FastKNN
+{
+public:
+    vector<double> input_vec;
+    NormalizerData <double> Normalizer_Data;
+    std::string mode_distance;
+    mat DataSet, data;
+    ucolvec labels;
+    rowvec input; 
+    int k;
+	void train(std::string filepath);
+    int Distance();
+	int Classify(std::string type_distance, rowvec input_user, unsigned k_input);
+};
+
+#endif 
+
 int FastKNN::Distance()
 {
         std::map<uword, int> Counter_of_classes;
@@ -87,6 +194,3 @@ int FastKNN::Classify(std::string type_distance, rowvec input_user, unsigned k_i
 
 	return 0;
 }
-
-
-
