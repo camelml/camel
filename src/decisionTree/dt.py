6655ba4e23e60a58418cf31f8b0e6fc7d373f408
harshit <harshitkhandelwal830@gmail.com>
2019-03-22 02:25:46 +0530

    decision tree compltete py

--- src/decisionTree/dt.py
+++ src/decisionTree/dt.py
@@ -34,3 +34,86 @@ data = pandas.DataFrame([
 
 data.columns = ["high_income", "age", "marital_status"]
 
+def calc_information_gain(data, split_name, target_name):
+    """
+    Calculate information gain given a data set, column to split on, and target
+    """
+
+    # Calculate the original entropy
+    original_entropy = calc_entropy(data[target_name])
+    
+    # Find the median of the column we're splitting
+    column = data[split_name]
+    median = column.median()
+    
+    # Make two subsets of the data, based on the median
+    left_split = data[column <= median]
+    right_split = data[column > median]
+    
+    # Loop through the splits and calculate the subset entropies
+    to_subtract = 0
+    for subset in [left_split, right_split]:
+        prob = (subset.shape[0] / data.shape[0]) 
+        to_subtract += prob * calc_entropy(subset[target_name])
+    
+    # Return information gain
+    return original_entropy - to_subtract
+
+
+def find_best_column(data, target_name, columns):
+    information_gains = []
+
+    # Loop through and compute information gains
+    for col in columns:
+        information_gain = calc_information_gain(data, col, "high_income")
+        information_gains.append(information_gain)
+
+    # Find the name of the column with the highest gain
+    highest_gain_index = information_gains.index(max(information_gains))
+    highest_gain = columns[highest_gain_index]
+    return highest_gain
+
+tree = {}
+nodes = []
+spaces = 20
+
+def id3(data, target, columns, tree):
+
+    print(data)
+    unique_targets = pandas.unique(data[target])
+    nodes.append(len(nodes) + 1)
+    tree["number"] = nodes[-1]
+
+    if len(unique_targets) == 1:
+        if 0 in unique_targets:
+            tree["label"] = 0
+        elif 1 in unique_targets:
+            tree["label"] = 1
+        return
+    
+    best_column = find_best_column(data, target, columns)
+    column_median = data[best_column].median()
+    
+    tree["column"] = best_column
+    tree["median"] = column_median
+    
+    left_split = data[data[best_column] <= column_median]
+    right_split = data[data[best_column] > column_median]
+    split_dict = [["left", left_split], ["right", right_split]]
+    
+    for name, split in split_dict:
+        print(name)
+        tree[name] = {}
+        id3(split, target, columns, tree[name])
+
+
+id3(data, "high_income", ["age", "marital_status"], tree)
+
+def pretty(d, indent=0):
+   for key, value in d.items():
+      print('\t' * indent + str(key))
+      if isinstance(value, dict):
+         pretty(value, indent+1)
+      else:
+         print('\t' * (indent+1) + str(value))
+pretty(tree)
