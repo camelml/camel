873f744ede331c94cd40e924cde3045a1c2e18df
harshit <harshitkhandelwal830@gmail.com>
2019-03-22 02:12:30 +0530

    decision tree

---
+++ src/decisionTree/dt.py
@@ -0,0 +1,36 @@
+import math
+import numpy
+import pandas
+
+def calc_entropy(column):
+    """
+    Calculate entropy given a pandas series, list, or numpy array.
+    """
+    # Compute the counts of each unique value in the column
+    counts = numpy.bincount(column)
+    # Divide by the total column length to get a probability
+    probabilities = counts / len(column)
+    
+    # Initialize the entropy to 0
+    entropy = 0
+    # Loop through the probabilities, and add each one to the total entropy
+    for prob in probabilities:
+        if prob > 0:
+            entropy += prob * math.log(prob) # Base e
+            # Change base to 2, 6 or 10.
+            # Example: math.log(prob, 2)
+            # The result will still be same.
+
+    return -entropy
+
+data = pandas.DataFrame([
+    [0,20,0],
+    [0,60,2],
+    [0,40,1],
+    [1,25,1],
+    [1,35,2],
+    [1,55,1]
+    ])
+
+data.columns = ["high_income", "age", "marital_status"]
+
