59556b041c5236c2347328645ea024d2bdf15259
kashish khullar <kkhullar7@gmail.com>
2019-04-05 21:24:59 +0530

    updated linear regression

--- include/LinearRegression.hpp
+++ include/LinearRegression.hpp
@@ -11,16 +11,22 @@ class LinearRegression
 private:
   mat X;
   mat y;
+  mat X_train;
+  mat y_train;
+  mat X_test;
+  mat y_test;
   mat dataSet;
   mat theta;
+  void split(const double, const double);
+
   // void normalize(mat mean, mat standardDeviation, mat data);
   mat gradientDescent(const mat &, const mat &, const double, const int);
   mat computeCost(const mat &X, const mat &y, const mat &theta);
 
 public:
-  LinearRegression(std::string const &);
-  void fit(const double, const int);
-  std::vector<double> predict(const std::string &);
+  LinearRegression(std::string const &, const double, const double);
+  void fit(const double &, const int &);
+  std::vector<double> predict();
 };
 
 #endif
