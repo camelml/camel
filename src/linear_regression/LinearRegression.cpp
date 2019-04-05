a75e2b22087c2e69ba1279f8a4025a1a2797ec0c
kashish khullar <kkhullar7@gmail.com>
2019-04-05 21:24:59 +0530

    updated linear regression

--- src/linear_regression/LinearRegression.cpp
+++ src/linear_regression/LinearRegression.cpp
@@ -2,16 +2,23 @@
 #include <iostream>
 typedef std::vector<double> stdvec;
 
-LinearRegression::LinearRegression(std::string const &path)
+LinearRegression::LinearRegression(std::string const &path, const double train, const double test)
 {
     dataSet.load(path);
-    // cout << std::fixed << dataSet << endl;
     y = dataSet.col(dataSet.n_cols - 1);
-    // cout << y << endl;
     X = dataSet.submat(0, 0, dataSet.n_rows - 1, dataSet.n_cols - 2);
-    // cout << X << endl;
     X = join_rows(randi<vec>(dataSet.n_rows).ones(), X);
-    normalise(X);
+    theta = mat(X.n_cols, 1).zeros();
+    split(train, test);
+}
+
+void LinearRegression::split(const double train, const double test)
+{
+    int trainIndex = X.n_rows * (train / (train + test)) - 1;
+    X_train = X.submat(0, 0, trainIndex, X.n_cols - 1);
+    X_test = X.submat(trainIndex + 1, 0, X.n_rows - 1, X.n_cols - 1);
+    y_train = y.submat(0, 0, trainIndex, y.n_cols - 1);
+    y_test = y.submat(trainIndex + 1, 0, y.n_rows - 1, y.n_cols - 1);
 }
 
 mat LinearRegression::computeCost(const mat &X, const mat &y, const mat &theta)
@@ -21,11 +28,10 @@ mat LinearRegression::computeCost(const mat &X, const mat &y, const mat &theta)
 
 mat LinearRegression::gradientDescent(const mat &X, const mat &y, const double alpha, const int iterations)
 {
-    theta = mat(X.n_cols, 1).zeros();
 
     for (int i = 0; i < iterations; i++)
     {
-        theta = theta - alpha * ((double)1 / (X.n_rows)) * X.t() * (X * theta - y);
+        theta = theta - alpha * ((double)1 / (X_train.n_rows)) * X_train.t() * (X_train * theta - y_train);
 
         // cout << computeCost(X, y, theta) << endl;
         //uncomment the above line if you want to to see how the cost converges.
@@ -36,20 +42,14 @@ mat LinearRegression::gradientDescent(const mat &X, const mat &y, const double a
     return theta;
 }
 
-std::vector<double> LinearRegression::predict(const std::string &path)
+std::vector<double> LinearRegression::predict()
 {
-    mat x_pred;
-    x_pred.load(path);
-    x_pred = join_rows(randi<vec>(x_pred.n_rows).ones(), x_pred);
-    cout << x_pred << endl;
-    cout << theta << endl;
-    mat y_pred = x_pred * theta;
+    mat y_pred = X_test * theta;
     cout << y_pred(0, 0) << endl;
     return conv_to<stdvec>::from(y_pred);
 }
 
-void LinearRegression::fit(const double alpha = 0.002, const int iterations = 10000)
+void LinearRegression::fit(const double &alpha = 0.002, const int &iterations = 10000)
 {
-    std::cout << "here" << std::endl;
     gradientDescent(X, y, alpha, iterations);
 }
